be0d69734a64c793d54c76ce387b574793f65a51
Alan Page <alandouglaspage@gmail.com>
2021-10-28 16:18:52 -0400

    Add on-screen status feedback to pimoroni graph example code

---
+++ blinkt/blinkt_graph.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import math
+import time
+
+import blinkt
+import random
+
+blinkt.set_clear_on_exit()
+
+
+def show_graph(v, r, g, b):
+    v *= blinkt.NUM_PIXELS
+    for x in range(blinkt.NUM_PIXELS):
+        if v < 0:
+            r, g, b = 0, 0, 0
+        else:
+            r, g, b = [int(min(v, 1.0) * c) for c in [r, g, b]]
+        blinkt.set_pixel(x, r, g, b)
+        v -= 1
+
+    blinkt.show()
+
+blinkt.set_brightness(0.033)
+
+try:
+    while True: # Run constantly
+#    for y in range(60): # Only run 60 iterations
+        t = time.time()
+#        print "time: ", t
+        v = (math.sin(t) + 1) / 2  # Get a value between 0 and 1
+              # Not just get a value between 0 and 1, get a sinewave of values "...sin(system time), 100 times per second"
+              # Repeating this makes the bar go up and down, as the 0 to 1 values are fed into the above function and are spread 
+              # across the eight LEDs...genius
+        st = (math.sin(t))
+#        print "math.sin(t): ", st
+
+#        print "(math.sin(t) + 1 / 2: ", v
+#        print round(v, 1)
+        dots = v * 10
+        print '* ' * int(dots) # Print an asterisk for each 0.1 rise in the sin(t) value
+#        bluevalue = random.randint(0, 15)
+#        redvalue = random.randint(0, 15)
+        show_graph(v, 150, 0, 90)
+        time.sleep(0.1)
+except KeyboardInterrupt:
+    pass
