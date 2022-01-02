db1f024490f70f3a6e7511343392bd99e7cd8a87
Alan Page <alandouglaspage@gmail.com>
2022-01-01 21:46:21 -0500

    Get period 2 temperature value

--- blinkt/therm_data2.py
+++ blinkt/therm_data2.py
@@ -4,4 +4,7 @@ import requests
 response = requests.get("http://api.weather.gov/gridpoints/OKX/51,69/forecast/")
 days = json.loads(response.text)
 
+name = days['properties']['periods'][2]['name']
+temp = days['properties']['periods'][2]['temperature']
 
+print(name + ": " + str(temp))
