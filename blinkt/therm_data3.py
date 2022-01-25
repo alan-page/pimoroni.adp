7735abb8001f47d774a787a19076f806977d72c5
Alan Page <alandouglaspage@gmail.com>
2022-01-25 17:41:08 -0500

    Changed to a more-Pythonic conditional chain...wish there was a switch statement in Python 2.7

--- blinkt/therm_data3.py
+++ blinkt/therm_data3.py
@@ -5,7 +5,10 @@ from blinkt import set_pixel, set_brightness, show, clear
 import time
 
 while True:
+    print "Trying the weather API..."
     response = requests.get("http://api.weather.gov/gridpoints/OKX/51,69/forecast/")
+    print "Add a try/catch here in case the request fails."
+    print " "
     days = json.loads(response.text)
 
     name = days['properties']['periods'][1]['name']
@@ -13,23 +16,31 @@ while True:
 
     print(name + ": " + str(temp))
 
-    if temp < 40:
-        print "It's going to be cold tomorrow. Better wear a coat."
+
+    if 41 < temp < 50:
+        print "It's going to be almost temperate. Probably sweater weather."
+        set_pixel(4, 0, 20, 15)
+        show()
+        time.sleep(5)
+        clear()
+
+    if 31 < temp < 40:
+        print "It's going to be cold. Better wear a coat."
         set_pixel(3, 0, 15, 15)
         show()
         time.sleep(5)
         clear() 
 
-    if temp < 30:
+    if 21 < temp < 30:
         print "Better make it a warm coat."
-        set_pixel(2, 0, 5, 15)
+        set_pixel(2, 0, 10, 15)
         show()
         time.sleep(5)
         clear()
 
-    if temp < 20:
+    if 11 < temp < 20:
         print "Remember that really heavy parka..."
-        set_pixel(1, 0, 10, 15)
+        set_pixel(1, 0, 5, 15)
         show()
         time.sleep(5)
         clear()
